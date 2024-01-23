2f8cdc715bdeff1dbcc13c907872eadf1e56eeab
dkme01 <dkme01@gmail.com>
2024-01-23 10:57:45 -0300

    Inicio curso java Rocketseat

---
+++ src/HelloWorld.java
@@ -0,0 +1,23 @@
+/*
+ * HelloWorld = Nome da minha classe
+ * public = Tipo de acesso da classe
+ * class = Tipo da classe
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        /*
+         * Números (int, double, float, long)
+         * Texto (string)
+         * Booleanos (boolean)
+         */
+
+        int dadoDoTipoInt = 10;
+        double dadoDoTipoDouble = 3.14;
+        float dadoDoTipoFloat = 3.14f;
+        long dadoDoTipoLong = 3190829037182731092L;
+        String dadoDoTipoString = "Colocar o meu texto";
+        boolean dadoDoTipoBoolean = false;
+
+    }
+}
