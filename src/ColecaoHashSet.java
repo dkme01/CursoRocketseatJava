8e10057f9314a64c4ddbeddcf4985cdc0e9429ac
dkme01 <dkme01@gmail.com>
2024-01-26 12:55:51 -0300

    HashSet

---
+++ src/ColecaoHashSet.java
@@ -0,0 +1,21 @@
+import java.util.HashSet;
+
+public class ColecaoHashSet {
+  public static void main(String[] args) {
+    HashSet<Integer> numeros = new HashSet<>();
+    numeros.add(10);
+    numeros.add(11);
+    numeros.add(12);
+    numeros.add(13);
+    numeros.add(14);
+    numeros.add(15);
+
+//    numeros.remove(12);
+//
+//    for(Integer numero : numeros) {
+//      System.out.println(numero);
+//    }
+
+    System.out.println(numeros.contains(14));
+  }
+}
